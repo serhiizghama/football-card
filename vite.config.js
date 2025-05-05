b5cf1d24c156a58f65fe06026f4692251f8b14b6
serhiizghama <zmrser@gmail.com>
2025-05-05 15:10:34 +0700

    Update package name and version in package.json; enhance Vite config with preview settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js or vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 4173,
+    allowedHosts: ['ballrush.online']
+  }
+});
