9dda9edae029c35162d6461a1f1f285af2f76051
serhiizghama <zmrser@gmail.com>
2025-05-08 19:05:50 +0700

    Refactor group info styling in UserProfile component for improved text alignment and whitespace handling

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -44,7 +44,7 @@ const UserProfile = () => {
                     />
                 </div>
                 <h1>{user.username}</h1>
-                <p className="group-info">
+                <p className="group-info" style={{ whiteSpace: 'nowrap', textAlign: 'center' }}>
                     Group:{' '}
                     <Link to={`/group/${groupId}`} className="group-link">
                         {user.groupName}
